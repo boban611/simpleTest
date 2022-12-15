c2a507007722c75b48e9f3f93eced6abca8ec95d
boban <boban.kankaras611@gmail.com>
2022-12-15 12:02:56 +0200

    removing not required fields

--- src/App.js
+++ src/App.js
@@ -1,50 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import { useCallback, useEffect } from 'react';
 
 function App() {
-  const getData = useCallback(async () => {
-    let response =await fetch('https://paulcamper.de/api/inventory/search?center_lat=52.504043&center_lng=13.393236&page_size=24');
-    let res = response.json();
-    console.log(res.Data.length);
-  },[]);
-  useEffect(() => {
-    getData();
-  },[]);
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-       import logo from './logo.svg';
-import './App.css';
-import { useCallback, useEffect } from 'react';
 
-function App() {
-  const getData = useCallback(async () => {
-    let response =await fetch('https://paulcamper.de/api/inventory/search?center_lat=52.504043&center_lng=13.393236&page_size=24');
-    let res = response.json();
-    console.log(res.Data.length);
-  },[]);
-  useEffect(() => {
-    getData();
-  },[]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
     </div>
   );
 }
